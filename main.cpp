a9ff03b6761fe7de347fde3b19db9f1eaf43fc12
Thbop <75861321+thbop@users.noreply.github.com>
2023-08-19 23:30:08 -0400

    Added LDA Absolute

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+    All of this code was typed by Thbop
+    References:
+        1. The initial inspiration and reference for this project was provided by Dave Poo's video:
+        https://youtu.be/qJgsuQoy9bc
+        2. https://web.archive.org/web/20200610195848/http://www.obelisk.me.uk/6502/
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,8 +18,10 @@ using u32 = unsigned int;
 struct Mem {
     static constexpr u32 MAX_MEM = 1024 * 64;
     /*
-        $0000 - $00FF - ZERO PAGE: Fast memory like registers
-        
+        $0000 - $00FF (256): ZERO PAGE - Fast memory like registers
+        $0100 - $01FF (256): System stack
+        $0200 - $FFF9 (65018): Regular RAM
+        $FFFA - $FFFF (6): Other stuff. *2 /architecture.html
     */
     Byte Data[MAX_MEM];
 
@@ -23,12 +33,16 @@ struct Mem {
 
     // Read 1 byte
     Byte operator[]( u32 Address ) const {
-        // assert here Address is < MAX_MEM
+        if (Address > 0xFFFF) {
+            printf("Memory operation reading address \"%x\" exceeds memory limit of $FFFF\n", Address);
+        }
         return Data[Address];
     }
     // write 1 byte
     Byte& operator[]( u32 Address ) {
-        // assert here Address is < MAX_MEM
+        if (Address > 0xFFFF) {
+            printf("Memory operation writing address \"%x\" exceeds memory limit of $FFFF\n", Address);
+        }
         return Data[Address];
     }
 
@@ -76,7 +90,7 @@ struct CPU {
         Word Data = memory[PC];
         PC++;
 
-        Data |= (memory[PC] << 8 );
+        Data |= (memory[PC] << 8 ); // Combines two Bytes into a Word
         PC++;
         Cycles -= 2;
 
@@ -88,12 +102,18 @@ struct CPU {
         Cycles--;
         return Data;
     }
+    Byte ReadByte( Word Address, u32& Cycles, Mem& memory ) {
+        Byte Data = memory[Address];
+        Cycles--;
+        return Data;
+    }
 
     // opcodes
     static constexpr Byte
         INS_LDA_IM = 0xA9,
         INS_LDA_ZP = 0xA5,
         INS_LDA_ZPX = 0xB5,
+        INS_LDA_ABS = 0xAD,
         INS_JSR = 0x20;
 
     void LDASetStatus() {
@@ -105,6 +125,7 @@ struct CPU {
     void Execute( u32 Cycles, Mem& memory ) {
         while (Cycles > 0) {
             Byte Ins = FetchByte( Cycles, memory );
+
             switch( Ins ) {
                 case INS_LDA_IM: {
                     Byte Value = FetchByte( Cycles, memory );
@@ -123,7 +144,12 @@ struct CPU {
                     A = ReadByte( ZeroPageAddr, Cycles, memory );
                     LDASetStatus();
                 } break;
-                
+                case INS_LDA_ABS: {
+                    Word AbsAddr = FetchWord( Cycles, memory );
+                    A = ReadByte( AbsAddr, Cycles, memory );
+                    LDASetStatus();
+                } break;
+
                 case INS_JSR: {
                     Word SubAddr = FetchWord( Cycles, memory );
                     memory.WriteWord( PC - 1, SP, Cycles );
@@ -144,16 +170,26 @@ struct CPU {
 
 int main() {
     Mem mem;
+
+
     CPU cpu;
     cpu.Reset( mem );
 
     // start - inline cheat code
-    mem[0xFFFC] = CPU::INS_LDA_ZP;
-    mem[0xFFFD] = 0x42;
-    mem[0x0042] = 0x84;
+    
+    mem[0xFFF9] = 43;
+
+    mem[0xFFFC] = CPU::INS_LDA_ABS;
+    mem[0xFFFD] = 0xF9;
+    mem[0xFFFE] = 0xFF;
+
     // end - inline cheat code
 
-    cpu.Execute( 3, mem );
+    cpu.Execute( 4, mem );
+
+    printf("%d\n", cpu.A);
+
+
 
 
     return 0;
