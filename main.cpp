f9fccccb47c6b12c16aa801b2b219fc94789f674
Thbop <75861321+thbop@users.noreply.github.com>
2024-01-14 12:24:02 -0500

    Finished the ascii table

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main( int argc, char *argv[] ) {
     const int WIDTH = 800, HEIGHT = 600;
 
 
-    char txt[] = "Rest, go to sleep.";
+    char txt[] = "W_PushText( txt[i] );";
     for (int i = 0; i < sizeof(txt); i++) {
         W_PushText( txt[i] );
     }
