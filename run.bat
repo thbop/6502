90168917f4aa6f263d3d59959747b7ea02fa2702
Thbop <75861321+thbop@users.noreply.github.com>
2024-02-12 22:46:22 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
+g++ -Isrc/include -IC:/mingw_dev_lib/SDL2/i686-w64-mingw32/include -LC:/mingw_dev_lib/SDL2/i686-w64-mingw32/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
 main
